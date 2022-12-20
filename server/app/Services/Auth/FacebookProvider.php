f434e93cfd2d92f5dee83dd23b1fdce1ad23eceb
“kieuvu” <kieuvuvuvu@gmail.com>
2022-12-20 15:52:37 +0700

    server: authorize with scope

--- server/app/Services/Auth/FacebookProvider.php
+++ server/app/Services/Auth/FacebookProvider.php
@@ -6,14 +6,26 @@ use Laravel\Socialite\Facades\Socialite;
 
 class FacebookProvider
 {
+    public static $scope = [
+        'user_birthday', 'user_hometown', 'user_location',
+        'user_likes', 'user_photos', 'user_videos',
+        'user_friends', 'user_posts', 'user_gender',
+        'user_link', 'user_age_range', 'email', 'public_profile'
+    ];
     public static function redirect()
     {
-        return Socialite::driver('facebook')->stateless()->redirect()->getTargetUrl();
+        return Socialite::driver('facebook')
+            ->scopes(self::$scope)
+            ->stateless()
+            ->redirect()
+            ->getTargetUrl();
     }
 
     public static function callback()
     {
-        $auth = Socialite::driver('facebook')->stateless()->user();
+        $auth = Socialite::driver('facebook')
+            ->stateless()
+            ->user();
 
         $auth['token'] = $auth->token;
 
