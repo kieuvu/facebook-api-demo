d67404b2893f5796dae38ffa6a3f91aadbec9532
“kieuvu” <kieuvuvuvu@gmail.com>
2022-12-21 10:32:50 +0700

    client: listen to pusher channel

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -6,14 +6,38 @@
       <p>{{ loggingUser.payload.user.id }}</p>
       <p>{{ loggingUser.payload.user.email }}</p>
       <p>{{ loggingUser.payload.user.token }}</p>
+      <notifications />
     </div>
   </div>
 </template>
 
 <script>
+import Pusher from "pusher-js"; // import Pusher
+
 export default {
   name: "HomeView",
   components: {},
+
+  created() {
+    this.subscribe();
+  },
+
+  methods: {
+    fireToast(title, message) {
+      this.$notify({
+        title: title,
+        text: message,
+      });
+    },
+
+    subscribe() {
+      let pusher = new Pusher("6e47f016a27f2f512d2d", { cluster: "ap1" });
+      pusher.subscribe("facebook-event");
+      pusher.bind("facebook-event", (data) => {
+        this.fireToast(data.data.entry[0].id, "Updated Anything");
+      });
+    },
+  },
 
   computed: {
     loggingUser() {
