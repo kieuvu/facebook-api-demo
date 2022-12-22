453af28c1bf5e86c7f46c9c93899bfa7bb6f39b8
“kieuvu” <kieuvuvuvu@gmail.com>
2022-12-22 16:43:54 +0700

    client: notif message

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="home text-center">
+  <div class="home p-5" style="width: 500px; overflow-wrap: break-word">
+    <div class="d-flex justify-content-end">
+      <button @click="logout()">Logout</button>
+    </div>
     <h1 class="text-danger">Homepage</h1>
     <div v-if="loggingUser">
-      <p>{{ loggingUser.payload.user.name }}</p>
-      <p>{{ loggingUser.payload.user.id }}</p>
-      <p>{{ loggingUser.payload.user.email }}</p>
-      <p>{{ loggingUser.payload.user.token }}</p>
+      <p><b>Username: </b>{{ loggingUser.payload.user.name }}</p>
+      <p><b>Id: </b>{{ loggingUser.payload.user.id }}</p>
+      <p><b>Email: </b>{{ loggingUser.payload.user.email }}</p>
+      <p><b>Token: </b>{{ loggingUser.payload.user.token }}</p>
       <notifications />
     </div>
   </div>
@@ -23,9 +26,14 @@ export default {
   },
 
   methods: {
-    fireToast(title, message) {
+    logout() {
+      this.$store.dispatch("logout");
+      this.$router.push("/auth");
+    },
+
+    fireToast(message) {
       this.$notify({
-        title: title,
+        title: "Notification",
         text: message,
       });
     },
@@ -34,7 +42,25 @@ export default {
       let pusher = new Pusher("6e47f016a27f2f512d2d", { cluster: "ap1" });
       pusher.subscribe("facebook-event");
       pusher.bind("facebook-event", (data) => {
-        this.fireToast(data.data.entry[0].id, "Updated Anything");
+        const userId = data.data.entry[0].id;
+        const eventAction = data.data.entry[0].changes[0].field;
+
+        const message = {
+          feed: function () {
+            return `User ${this.userId} updated Something`;
+          },
+          photos: function () {
+            return `User ${this.userId} uploaded a new photo`;
+          },
+          status: function () {
+            return `User ${this.userId} uploaded a new post`;
+          },
+          friends: function () {
+            return `User ${this.userId} has a new friend`;
+          },
+        };
+
+        this.fireToast(message[eventAction].call({ userId }));
       });
     },
   },
