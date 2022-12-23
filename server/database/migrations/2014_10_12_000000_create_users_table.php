0bda32f0b7f1c060b8613efc2cd5343c4202d18b
“kieuvu” <kieuvuvuvu@gmail.com>
2022-12-23 11:05:52 +0700

    server: create user model

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('facebook_id');
+            $table->string('email')->nullable();
+            $table->string('avatar_url');
+            $table->string('gender')->nullable();
+            $table->string('token')->nullable();
             $table->timestamps();
         });
     }
